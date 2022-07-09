c295c97e96e9c4f430a05b1402fa3a057306d122
Matthieu Lienart <matthieu.lienart@amanox.ch>
2022-07-09 16:53:18 +0200

    Added possibility to deploy the stack based on git branch name instead of passing it as an argument

--- bin/data-ingestion.ts
+++ bin/data-ingestion.ts
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { DataIngestionPipelineStack, StageType } from '../lib/pipeline-stack';
+import { DataIngestionPipelineStack } from '../lib/pipeline-stack';
+import { getCurrentBranchName } from '../lib/git-branch'
 
 const app = new cdk.App();
+let currentBranch = getCurrentBranchName() || 'unknown';
+console.log('Current branch name: ', currentBranch);
+
 new DataIngestionPipelineStack(app, 'DataIngestionPipelineStack', {
-  prefix: '3mr50x-mlops-rdi',
   repoName: 'amanoxsolutions/mlops-realtime-data-ingestion',
-  stage: StageType.DEV,
   codestarConnectionName: 'mlops-realtime-data-ingestion',
+  // The code in the current active branch will be the one deployed by the pipeline
+  // The branch name is used to create a Hash for all the resources created by the pipeline
+  // This allows to deploy multiple versions of the same code in parallel the same AWS Account
+  branchName: currentBranch,
 });
 
 app.synth();
--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -3,52 +3,56 @@ import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { RealtimeDataIngestionStage } from './pipeline-stage';
 import { CodestarConnection } from './codestar-connection';
+import { getShortHashFromString } from './git-branch';
 
-export enum StageType {
-  PROD = 'prod',
-  TEST = 'test',
-  DEV = 'dev',
-}
 
 export interface DataIngestionPipelineStackProps extends StackProps {
-  readonly prefix: string;
+  readonly prefix?: string;
   readonly repoName: string;
   readonly codestarConnectionName: string;
-  readonly stage?: StageType;
+  readonly branchName: string;
 }
 
 export class DataIngestionPipelineStack extends Stack {
-
   constructor(scope: Construct, id: string, props: DataIngestionPipelineStackProps) {
     super(scope, id, props);
 
-    const stage = props.stage || StageType.TEST;
-    const branchName = 'main';
-    if (stage == StageType.DEV) {
-      const branchName = 'develop';
+    // Get the first 6 characters of the hash value computed from the Git branch name
+    // and use it in the prefix of all the resource names
+    const branchHash = getShortHashFromString(props.branchName);
+    console.log('Hash value computed from the branch name and used for resource names: ', branchHash);
+    let prefix = `mlops-rdi-${branchHash}`;
+    if (props.prefix) {
+      prefix = `${props.prefix}-${branchHash}`;
     }
+    // Create a unique suffix based on the AWS account number to be used for resources
+    // this is used for S3 bucket bucket names for example
+    const uniqueSuffix = getShortHashFromString(this.account, 8);
+    console.log('unique resource Suffix: ', uniqueSuffix);
 
     const codestarConnection = new CodestarConnection(this, 'CsConnection', {
-      prefix: props.prefix,
+      prefix: prefix,
       name: props.codestarConnectionName
     });
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: `${props.prefix}-${stage}-pipeline`,
+      pipelineName: `${prefix}-pipeline`,
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.connection(
           props.repoName, 
-          branchName,
+          props.branchName,
           { connectionArn: codestarConnection.arn }
         ),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+        // The CodePipeline must pull the code from the git branch used by the user
+        commands: [`git checkout ${props.branchName}`, 'cat .git/HEAD', 'npm ci', 'npm run build', 'npx cdk synth']
       }),
       dockerEnabledForSynth: true,
     });
     pipeline.node.addDependency(codestarConnection);
 
     pipeline.addStage(new RealtimeDataIngestionStage(this, "Stage", {
-      prefix: props.prefix,
+      prefix: prefix,
+      uniqueSuffix: uniqueSuffix,
     }));
 
   }
