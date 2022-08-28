ab4f5e659875ac3357dd1fc83445f6eb6f2e5d7e
Matthieu Lienart <matthieu.lienart@amanox.ch>
2022-08-28 17:55:53 +0200

    Update data filtering function

--- resources/lambdas/filter_duplicates/main.py
+++ resources/lambdas/filter_duplicates/main.py
@@ -2,6 +2,9 @@ import base64
 import io
 import json
 import os
+import time
+
+from datetime import timedelta
 
 import boto3
 from aws_lambda_powertools import Logger, Metrics, Tracer
@@ -12,6 +15,8 @@ tracer = Tracer()
 
 DYNAMODB_SEEN_TABLE_NAME = os.environ.get("DYNAMODB_SEEN_TABLE_NAME")
 HASH_KEY_NAME = os.environ.get("HASH_KEY_NAME")
+TTL_ATTRIBUTE_NAME = os.environ.get("TTL_ATTRIBUTE_NAME")
+DDB_ITEM_TTL_HOURS = int(os.environ.get("DDB_ITEM_TTL_HOURS"))
 
 # TODO introduce boto3 session retry-config
 dynamodb_resource = boto3.resource("dynamodb")
@@ -23,40 +28,42 @@ table_of_seen_items = dynamodb_resource.Table(DYNAMODB_SEEN_TABLE_NAME)
 @tracer.capture_lambda_handler
 def lambda_handler(event, context):
     result = []
-    for raw_record in event["Records"]:
-        raw_data = raw_record["kinesis"]["data"]
-        # base64 decode the data
-        record = base64.b64decode(raw_data)
-        event_hash = record[HASH_KEY_NAME]
+    for raw_record in event["records"]:
+        raw_data = raw_record["data"]
+        # base64 decode the data and load the JSON data
+        record = json.loads(base64.b64decode(raw_data))
 
-        # only create item if it does not exist
-        # https://stackoverflow.com/a/55110463/429162
-        try:
-            table_of_seen_items.put_item(
-                Item={
-                    HASH_KEY_NAME: event_hash,
-                    "seen": 0
-                },
-                ConditionExpression=Attr(HASH_KEY_NAME).not_exists()
-            )
-        except dynamodb_resource.meta.client.exceptions.ConditionalCheckFailedException:
-            # if ConditionExpression resolves to false, the query will return a 400 err
-            continue
+        for transaction in record["detail"]["txs"]:
+            transaction_hash = transaction[HASH_KEY_NAME]
 
-        table_of_seen_items.update_item()
-        try:
-            # using write condition idempotence
-            table_of_seen_items.update_item(
-                Key={HASH_KEY_NAME: event_hash},
-                ConditionExpression=Attr("seen").eq(0),
-                # Can not use both expression and non-expression parameters in the same request and we need ConditionExpression
-                UpdateExpression=f"SET seen=:val",
-                ExpressionAttributeValues={
-                    ":val": 1
-                },
-                ReturnValues="UPDATED_NEW")
-        except table_of_seen_items.meta.client.exceptions.ConditionalCheckFailedException as e:
-            logger.exception("been there seen that")
-        else:
-            result.append(record)
-        return result  # TODO validate needed result format
+            # only create item if it does not exist
+            # https://stackoverflow.com/a/55110463/429162
+            try:
+                table_of_seen_items.put_item(
+                    Item={
+                        HASH_KEY_NAME: transaction_hash,
+                        "seen": 0,
+                        TTL_ATTRIBUTE_NAME: int(time.time() + timedelta(hours=DDB_ITEM_TTL_HOURS).total_seconds())
+                    },
+                    ConditionExpression=Attr(HASH_KEY_NAME).not_exists()
+                )
+            except dynamodb_resource.meta.client.exceptions.ConditionalCheckFailedException:
+                # if ConditionExpression resolves to false, the query will return a 400 err
+                continue # TODO is this continue or pass ?
+
+            try:
+                # using write condition idempotence
+                table_of_seen_items.update_item(
+                    Key={HASH_KEY_NAME: transaction_hash},
+                    ConditionExpression=Attr("seen").eq(0),
+                    # Can not use both expression and non-expression parameters in the same request and we need ConditionExpression
+                    UpdateExpression=f"SET seen=:val",
+                    ExpressionAttributeValues={
+                        ":val": 1
+                    },
+                    ReturnValues="UPDATED_NEW")
+            except table_of_seen_items.meta.client.exceptions.ConditionalCheckFailedException as e:
+                logger.exception("been there seen that")
+            else:
+                result.append(transaction)
+            return result  # TODO validate needed result format
