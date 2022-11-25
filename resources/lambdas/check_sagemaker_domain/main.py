b9ec3c778f8888ce4fc2340bd91604a152d343dd
Matthieu Lienart <matthieu.lienart@amanox.ch>
2022-11-25 21:16:36 +0100

    Fix the Lambda checking for sagemaker domains

--- resources/lambdas/check_sagemaker_domain/main.py
+++ resources/lambdas/check_sagemaker_domain/main.py
@@ -12,7 +12,7 @@ PHYSICAL_ID = "CustomResourceToCheckForExistingSageMakerStudioDomain"
 
 def lambda_handler(event, context):
     response_data = {"SageMakerDomains": ""}
-    sagemaker_domain_list = []
+    sagemaker_domains_list = []
     try:
         request = event.get("RequestType").lower()
         logger.info(f"Type of request: {request}")
@@ -20,13 +20,13 @@ def lambda_handler(event, context):
         # As of now there can only be one SageMaker Studio Domain
         # per AWS Account and Region but let's try to be future proof
         # by following the function specification and search for the entire list of domains
-        next_token, sagemaker_domain_list = list_existing_sagemake_domains()
+        next_token, sagemaker_domains_list = list_existing_sagemaker_domains()
         while next_token:
-            next_token, additional_domain_list = list_existing_sagemake_domains(next_token)
-            sagemaker_domain_list.extend(additional_domain_list)         
+            next_token, additional_domain_list = list_existing_sagemaker_domains(next_token)
+            sagemaker_domains_list.extend(additional_domain_list)         
         # we can only return data as simple key-value pairs
         # so we convert the list of domains to a comma separated string
-        sagemaker_domains = ",".join(sagemaker_domain_list)
+        sagemaker_domains = ",".join(sagemaker_domains_list)
         response_data = {"SageMakerDomains": sagemaker_domains}
     except Exception as e:
         logger.exception(e)
@@ -34,14 +34,15 @@ def lambda_handler(event, context):
     else:
         cfnresponse.send(event, context, cfnresponse.SUCCESS, response_data, physicalResourceId=PHYSICAL_ID)
 
-def list_existing_sagemake_domains(next_token: str = None) -> Tuple[str, List[str]]:
+def list_existing_sagemaker_domains(next_token: str = None) -> Tuple[str, List[str]]:
     """This function list existing SageMaker Studio Domains
     """
     if not next_token:
         sm_response = sagemaker.list_domains()
     else:
         sm_response = sagemaker.list_domains(NextToken=next_token)
-    domains_list = sm_response.get("Domains")
+    # Get all the domain naimes in the domain list
+    domains_list = [domain.get("DomainName") for domain in sm_response.get("Domains", [])]
     next_token = sm_response.get("NextToken")
     logger.info(f"Existing SageMaker Studio Domains: {domains_list}")
     return next_token, domains_list
