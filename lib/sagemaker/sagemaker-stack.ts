3992b2d23f5f4964791349b36eb6f6b9f050438c
Matthieu Lienart <matthieu.lienart@amanox.ch>
2022-11-25 21:52:17 +0100

    Add feature sotre

--- lib/sagemaker/sagemaker-stack.ts
+++ lib/sagemaker/sagemaker-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps, RemovalPolicy, Duration, Size } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { IVpc, SubnetType } from 'aws-cdk-lib/aws-ec2';
 import { RDISagemakerStudio } from './sagemaker';
+import { RDIFeatureStore } from './feature-store';
 
 export interface SagemakerStackProps extends StackProps {
   readonly prefix: string;
@@ -15,7 +16,8 @@ export class SagemakerStack extends Stack {
   public readonly prefix: string;
   public readonly s3Suffix: string;
   public readonly removalPolicy: RemovalPolicy;
-  public readonly sagemakerStudio: RDISagemakerStudio;
+  public readonly studio: RDISagemakerStudio;
+  public readonly featureStore: RDIFeatureStore;
 
   constructor(scope: Construct, id: string, props: SagemakerStackProps) {
     super(scope, id, props);
@@ -24,11 +26,15 @@ export class SagemakerStack extends Stack {
     this.s3Suffix = props.s3Suffix;
     this.removalPolicy = props.removalPolicy || RemovalPolicy.DESTROY;
 
-    this.sagemakerStudio = new RDISagemakerStudio(this, 'sagemakerStudio', {
+    this.studio = new RDISagemakerStudio(this, 'sagemakerStudio', {
       prefix: this.prefix,
       dataBucketArn: props.dataBucketArn,
       vpcId: props.vpc.vpcId,
       subnetIds: props.vpc.selectSubnets({ subnetType: SubnetType.PUBLIC }).subnetIds,
+    });
+
+    this.featureStore = new RDIFeatureStore(this, 'featureStore', {
+      prefix: this.prefix,
     });
   }
 }
