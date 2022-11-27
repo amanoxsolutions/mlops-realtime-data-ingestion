99427c903dd4e53a86455b3da8e63c58b051ad0d
Matthieu Lienart <matthieu.lienart@amanox.ch>
2022-11-27 19:24:44 +0100

    Change function to check for sagemaker domain since there can be only one

--- lib/sagemaker/sagemaker.ts
+++ lib/sagemaker/sagemaker.ts
@@ -61,13 +61,13 @@ export class RDISagemakerStudio extends Construct {
       serviceToken: customResourceLambda.functionArn,
     });
     // Get the list of {domainName, domainId} from the custom resource output SageMakerDomains attribute
-    const sagemakerDomains = JSON.parse(customResource.getAtt('SageMakerDomains').toString());
+    this.domainName = customResource.getAtt('SageMakerDomainName').toString();
+    this.domainId = customResource.getAtt('SageMakerDomainId').toString();
     // Should we need to create a new domain, we need a name for it
     const thisDomainName = `${this.prefix}-sagemaker-studio-domain`;
 
     // Create/Update/Delete SageMaker Studio Resources only if there is no domain already
-    // or the domain is the one in the list
-    if (sagemakerDomains.length === 0 || sagemakerDomains.includes(thisDomainName)) {
+    if (this.domainName === '') {
       // Create the IAM Role for SagMaker Studio
       this.role = new Role(this, 'studioRole', {
           roleName: `${this.prefix}-sagemaker-studio-role`,
@@ -108,11 +108,6 @@ export class RDISagemakerStudio extends Construct {
       });
       this.domainName = domain.domainName;
       this.domainId = domain.attrDomainId;
-    } else {
-      // If there is an existing domain, we need to get the name and ID from the list
-      // Ath the moment there is only one domain per account and region
-      this.domainName = sagemakerDomains[0].name;
-      this.domainId = sagemakerDomains[0].id;
     }
 
     // 
--- resources/lambdas/check_sagemaker_domain/main.py
+++ resources/lambdas/check_sagemaker_domain/main.py
@@ -1,9 +1,7 @@
 import os
-import json
 import boto3
 import logging
 import lib.cfnresponse as cfnresponse
-from typing import List, Tuple
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -12,37 +10,26 @@ sagemaker = boto3.client("sagemaker")
 PHYSICAL_ID = "CustomResourceToCheckForExistingSageMakerStudioDomain"
 
 def lambda_handler(event, context):
-    response_data = {"SageMakerDomains": ""}
+    response_data = {
+        "SageMakerDomainName": "",
+        "SageMakerDomainId": ""
+    }
     sagemaker_domains_list = []
     try:
         request = event.get("RequestType").lower()
         logger.info(f"Type of request: {request}")
-
         # As of now there can only be one SageMaker Studio Domain
-        # per AWS Account and Region but let's try to be future proof
-        # by following the function specification and search for the entire list of domains
-        next_token, sagemaker_domains_list = list_existing_sagemaker_domains()
-        while next_token:
-            next_token, additional_domain_list = list_existing_sagemaker_domains(next_token)
-            sagemaker_domains_list.extend(additional_domain_list)         
-        # we can only return data as simple key-value pairs
-        # so we convert the list of domains to a string
-        response_data = {"SageMakerDomains": json.dumps(sagemaker_domains_list)}
+        sm_response = sagemaker.list_domains()
     except Exception as e:
         logger.exception(e)
         cfnresponse.send(event, context, cfnresponse.FAILED, response_data, physicalResourceId=PHYSICAL_ID)
     else:
+        domains_list = sm_response.get("Domains", [])
+        if domains_list:
+            response_data["SageMakerDomainName"] = domains_list[0].get("DomainName")
+            response_data["SageMakerDomainId"] = domains_list[0].get("DomainId")
+            logger.info(f"Existing SageMaker Studio Domain: {response_data}")
+        else:
+            logger.info("No SageMaker Studio Domain found")
         cfnresponse.send(event, context, cfnresponse.SUCCESS, response_data, physicalResourceId=PHYSICAL_ID)
 
-def list_existing_sagemaker_domains(next_token: str = None) -> Tuple[str, List[str]]:
-    """This function list existing SageMaker Studio Domains
-    """
-    if not next_token:
-        sm_response = sagemaker.list_domains()
-    else:
-        sm_response = sagemaker.list_domains(NextToken=next_token)
-    # Get all the domain naimes in the domain list
-    domains_list = [{"name": domain.get("DomainName"), "id": domain.get("DomainId")} for domain in sm_response.get("Domains", [])]
-    next_token = sm_response.get("NextToken")
-    logger.info(f"Existing SageMaker Studio Domains: {domains_list}")
-    return next_token, domains_list
