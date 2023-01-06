559f7d1d966f9bf1abd6ec666bdd8729e816ae5e
Matthieu Lienart <matthieu.lienart@amanox.ch>
2023-01-06 19:24:09 +0100

    Fix logging

--- resources/lambdas/cleanup_sagemaker_studio/main.py
+++ resources/lambdas/cleanup_sagemaker_studio/main.py
@@ -71,10 +71,10 @@ def delete_sagemaker_studio_apps(domain_id: str):
     logger.info(f"Deleting all the user created SageMaker Studio apps for the domain {SAGEMAKER_DOMAIN_ID}")
     for app in apps:
         app_name = app.get("AppName")
-        logger.info(f"Deleting the user created SageMaker Studio app {app_name}")
         # The app created by the SageMaker stack will be automatically destroyed
         # Here we are deleting the apps created by the user
         if app_name != SAGEMAKER_APP_NAME:
+            logger.info(f"Deleting the user created SageMaker Studio app {app_name}")
             sagemaker.delete_app(
                 DomainId=domain_id,
                 UserProfileName=SAGEMAKER_USER_PROFILE,
