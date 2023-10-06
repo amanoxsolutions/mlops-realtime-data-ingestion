addbd1272cad56ede71ef613ae05888b9a065f4e
Raphael Eymann <raphael.eymann@amanox.ch>
2023-10-06 09:07:51 +0200

    improve lambda function/change filename

--- resources/lambdas/start_kinesis_app/main.py
+++ resources/lambdas/start_kinesis_app/main.py
@@ -8,27 +8,47 @@ client = boto3.client('kinesisanalytics')
 application_name = os.environ['KINESIS_ANALYTICS_NAME']
 input_starting_position = os.environ['INPUT_STARTING_POSITION']
 
+
 def lambda_handler(event, context):
     helper(event, context)
-    
+
+
 @helper.create
 def create(event, context):
-    kinesis_analytics = client.describe_application(
-        ApplicationName=application_name
-    )
-    
-    response = client.start_application(
-        ApplicationName=application_name,
-        InputConfigurations=[
-            {
-                'Id': kinesis_analytics["ApplicationDetail"]["InputDescriptions"][0]["InputId"],
-                'InputStartingPositionConfiguration': {
-                    'InputStartingPosition': input_starting_position
-                }
-            },
-        ]
-    )
-    
+    try:
+        kinesis_analytics = client.describe_application(
+            ApplicationName=application_name
+        )
+
+        response = client.start_application(
+            ApplicationName=application_name,
+            InputConfigurations=[
+                {
+                    'Id': kinesis_analytics["ApplicationDetail"]["InputDescriptions"][0]["InputId"],
+                    'InputStartingPositionConfiguration': {
+                        'InputStartingPosition': input_starting_position
+                    }
+                },
+            ]
+        )
+
+        return {
+            'Status': 'SUCCESS',
+            'Reason': '',
+            'LogicalResourceId': event.LogicalResourceId,
+            'RequestId': event.RequestId,
+            'StackId': event.StackId
+        }
+    except Exception as error:
+        return {
+            'Status': 'FAILED',
+            'Reason': json.dumps(error),
+            'LogicalResourceId': event.LogicalResourceId,
+            'RequestId': event.RequestId,
+            'StackId': event.StackId
+        }
+
+
 @helper.update
 def update(event, context):
     try:
@@ -47,9 +67,26 @@ def update(event, context):
             'RequestId': event.RequestId,
             'StackId': event.StackId
         }
-        
+
+
 @helper.delete
 def delete(event, context):
-    response = client.stop_application(
-        ApplicationName=application_name
-    )
+    try:
+        # response = client.stop_application(
+        #    ApplicationName=application_name
+        # )
+        return {
+            'Status': 'SUCCESS',
+            'Reason': '',
+            'LogicalResourceId': event.LogicalResourceId,
+            'RequestId': event.RequestId,
+            'StackId': event.StackId
+        }
+    except Exception as error:
+        return {
+            'Status': 'FAILED',
+            'Reason': json.dumps(error),
+            'LogicalResourceId': event.LogicalResourceId,
+            'RequestId': event.RequestId,
+            'StackId': event.StackId
+        }
