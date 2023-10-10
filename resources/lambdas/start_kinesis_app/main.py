fb62d191733fb521fef06399afb9409b09f86a66
Raphael Eymann <raphael.eymann@amanox.ch>
2023-10-10 11:13:37 +0200

    add custom resource logging for start kinesis

--- resources/lambdas/start_kinesis_app/main.py
+++ resources/lambdas/start_kinesis_app/main.py
@@ -1,9 +1,10 @@
+from aws_lambda_powertools import Logger
 from crhelper import CfnResource
-import json
 import boto3
 import os
 
 helper = CfnResource()
+logger = Logger()
 client = boto3.client('kinesisanalytics')
 application_name = os.environ['KINESIS_ANALYTICS_NAME']
 input_starting_position = os.environ['INPUT_STARTING_POSITION']
@@ -13,6 +14,9 @@ def lambda_handler(event, context):
     
 @helper.create
 def create(_, __):
+    logger.info("Got create event - starting Kinesis analytics application")
+    logger.info(f"Kinesis analytics application name: {application_name}")
+    logger.info(f"Input starting position: {input_starting_position}")
     kinesis_analytics = client.describe_application(
         ApplicationName=application_name
     )
@@ -31,8 +35,8 @@ def create(_, __):
     
 @helper.update
 def update(_, __):
-    pass
+    logger.info("Got update event - nothing todo")
         
 @helper.delete
 def delete(_, __):
-    pass
+    logger.info("Got delete event - nothing todo")
